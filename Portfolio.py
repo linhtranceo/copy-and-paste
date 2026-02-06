e0071a04b178ffa128c2fb52ef25b94e045669bb
linhtranceo <157328894+linhtranceo@users.noreply.github.com>
2026-02-06 17:15:22 -0500

    copy and paste

--- Portfolio.py
+++ Portfolio.py
@@ -40,23 +40,47 @@ def education_section(education_data, course_data):
         "skills": "What I Learned"},
         hide_index=True,
         )
-    st.write("--")
+    st.write("---")
     
-    
-
 education_section(info.education_data, info.course_data)
 
 #Professional Experience
 
 def experience_section(experience_data):
     st.header("Professional Experience")
-    for job_title,(job_description, image) in experience_data.item():
-        expnder = st.expander(f"{job_title}")
-        expander:image(image,width=250)
+    for job_title, (job_description,image)in experience_data.items():
+        expander = st.expander(f"{job_title}")
+        expander.image(image,width=250)
         for bullet in job_description:
             expander:write(bullet)
-    st.write("--")
-experience_section(info_experience_data)
+    st.write("---")
+experience_section(info.experience_data)
+
+#Projects
+
+def project_section(projects_data):
+    st.header("Projects")
+    for project_name,project_description in projects_data.items():
+        expander=st.expander(f"{project_name}")
+        expander.write(project_description)
+    st.write("---")
+project_section(info.projects_data)
+
+#Skills
+
+def skills_section(programming_data,spoken_data):
+    st.header("Skills")
+    st.subheader("Programming Languages")
+    for skill,percentage in programming_data.items():
+        st.write(f"{skill}{info.programming_icons.get(skill,'')}")
+        st.progress(percentage)
+    st.subheader("Spoken Languages")   
+    for spoken,proficiency in spoken_data.items():
+        st.write(f"{spoken}{info.spoken_icons.get(spoken,'')}:{proficiency}")
+        
+    st.write("---")
+skills_section(info.programming_data,info.spoken_data)
+    
 
 
 
